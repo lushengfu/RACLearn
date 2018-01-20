8c7218b082d945bc53631f4758cc679b4ab6e082
lushengfu <cylushengfu@163.com>
2018-01-20 09:59:36 +0800

    初始化项目

---
+++ RACLearn/RACLearn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RACLearn
+//
+//  Created by happy on 2018/1/20.
+//  Copyright © 2018年 happy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ RACLearn/RACLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACLearn
+//
+//  Created by happy on 2018/1/20.
+//  Copyright © 2018年 happy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
